74317ce4a0ee8b2e7ce86464caae2e364600dd08
Ana Beatriz Romero <133364091+Anabia-05@users.noreply.github.com>
2025-03-26 16:25:31 +0000

    inicio tela pomodoro

---
+++ pomodojo/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import logoPomodojoSF from '../assets/logoPomodojoSF.png'
+
+export  default function Header(){
+    <header>
+       <img src={logoPomodojoSF}  alt=""/>
+       <h1>PomoDojo</h1>
+   </header>
+}
