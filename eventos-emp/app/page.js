9757525a58a21da4bba7592807682c3e8a7efd41
pedroMB03 <pedrombunicap@gmail.com>
2025-04-07 17:11:23 -0300

    correção

--- eventos-emp/app/page.js
+++ eventos-emp/app/page.js
@@ -4,7 +4,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="container">
-
       <span className="icone" style={{ "--i": 13 }}></span>
       <span className="icone" style={{ "--i": 29 }}></span>
       <span className="icone" style={{ "--i": 7 }}></span>
@@ -31,15 +30,15 @@ export default function Home() {
       <span className="icone" style={{ "--i": 9 }}></span>
       <span className="icone" style={{ "--i": 72 }}></span>
 
-
       <div className="conteudo">
-
         <h2>Bem vindo ao</h2>
         <h1>Gerenciador de Eventos</h1>
         <h2>da sua empresa!</h2>
 
         <div className="acessar">
-          <button>Acessar</button>
+          <button>
+            <Link href="/eventos">Acessar</Link>
+          </button>
         </div>
       </div>
     </div>
--- projeto1/src/components/Eventos.jsx
+++
@@ -1,7 +0,0 @@
-export function Eventos({ evento, onDeleteClick }) {
-    return (
-        <Link href={`/app/${evento.objectID}`}>
-              <strong>{evento.NomeEvt}</strong>
-        </Link>
-    );
-}
